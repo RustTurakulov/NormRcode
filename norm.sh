e74fc6dcf9f981853bec86c84b4c5f29a8c68d24
Rust Turakulov <hoohaah@gmail.com>
2021-04-21 11:08:12 -0400

    R Normalization and biowulf script for it

---
+++ norm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --exclusive \
+#SBATCH --ntasks=28 \
+#SBATCH --mem=247g \
+#SBATCH --ntasks-per-core=28 \
+#SBATCH --gres=lscratch:100 \
+#SBATCH --time 24:00:00
+
+## Sbatch for running Drew's normalization protocol for idat 
+## Check nodes: freen |  Singularity container:  rbox4_v0.sif 
+
+cd /data/MDATA
+export TMPDIR=/lscratch/$SLURM_JOB_ID
+export SINGULARITYENV_THREADS=26
+gpfs_dirs="$(echo /gs* | tr ' ' ',')"
+export SINGULARITY_BINDPATH="/vf,${gpfs_dirs},/spin1,/data,/lscratch,/scratch,/fdb"
+module load singularity
+singularity exec  NormRcode/rbox_v0.sif /usr/bin/Rscript   NormRcode/normalization.R
