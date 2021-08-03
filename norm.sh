d6dee6b9e48a660dbb4e4a520dff443fcbf97f34
Rust Turakulov <hoohaah@gmail.com>
2021-08-03 13:33:12 -0400

    New version of normalization based on Drew mastersheet database.:wq

--- norm.sh
+++ norm.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
+#SBATCH --partition=largemem \
+#SBATCH --mem=1507g \
 #SBATCH --exclusive \
-#SBATCH --ntasks=28 \
-#SBATCH --mem=247g \
-#SBATCH --ntasks-per-core=1 \
-#SBATCH --gres=lscratch:200 \
+#SBATCH --ntasks=144 \
+#SBATCH --ntasks-per-core=2 \
+#SBATCH --gres=lscratch:300 \
 #SBATCH --time 24:00:00
 
 ## Sbatch for running Drew's normalization protocol for idat 
 ## Check nodes: freen |  Singularity container:  rbox4_v0.sif 
-
+echo "Parameters line parsed: $1"; 
+umask 007
 cd /data/MDATA
 export TMPDIR=/lscratch/$SLURM_JOB_ID
-export SINGULARITYENV_THREADS=26
+export SINGULARITYENV_THREADS=140
 gpfs_dirs="$(echo /gs* | tr ' ' ',')"
 export SINGULARITY_BINDPATH="/vf,${gpfs_dirs},/spin1,/data,/lscratch,/scratch,/fdb"
 module load singularity
-singularity exec  NormRcode/rbox_v0.sif /usr/bin/Rscript NormRcode/normalization.R $1
+singularity exec  NormRcode/rbox_v0.sif /usr/bin/Rscript NormRcode/pancancer_pipeline_NORM.R $1
